fbc7d62ad1e1c87670aedb0398bd45915be3486d
mpaydar <m21newyork@gmail.com>
2022-08-02 17:27:48 -0400

    made better time complexity code

--- loops.py
+++ loops.py
@@ -2,16 +2,31 @@
 
 
 
-
+a=[]
 nums=[3,3]
 target=6
 
 
 # Time complexity=n^2
-a=[]
-for i in range(len(nums)):
-  for j in range(i+1,len(nums)):
-      if nums[i] + nums [j] == target:
-        a=[i,j]
-print(a)
+# for i in range(len(nums)):
+#   for j in range(i+1,len(nums)):
+#       if nums[i] + nums [j] == target:
+#         a=[i,j]
+# print(a)
+#--------------------------------------------------------------------------------------------------------------------
+# dictionaries keys are unique: so we will use those values as key, their index in the list as their value
+# at any point we are going through the list, if we see the number we are interested is in the dictionary
+#  we will use the difference , which an actual key in dictionary to get the value(index), as well as the index of the value
+# we have in hand.... 
+my_dictionary={}
+for index,value in enumerate(nums):
+    difference=target-value
+    if value in my_dictionary:
+        print (my_dictionary[difference],index)
+    else:
+        my_dictionary[value]=index
+
+
+
+
               
