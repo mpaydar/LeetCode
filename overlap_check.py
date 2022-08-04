6c087facaad9bc2268178e2d24533b0e3f2dbb1b
mpaydar <m21newyork@gmail.com>
2022-08-04 18:07:21 -0400

    removed unneccessary print statement

--- overlap_check.py
+++ overlap_check.py
@@ -6,9 +6,6 @@ def evaluation(x,y):
     return x<y
 
 
-
-
-
 def over_lap_check(numbers):
     list_check1=[]
     list_check2=[]
@@ -17,17 +14,10 @@ def over_lap_check(numbers):
     min_value=0
     overlap_counter=0
     for n in range(len(numbers)):
-
-        number_of_evaluated=len(list_check1)+len(list_check2)
-       
+        number_of_evaluated=len(list_check1)+len(list_check2) 
         if number_of_evaluated==4:        
             max_value=max(list_check1)
-            print("max_value:",max_value)
-
             min_value=min(list_check2)
-            print("min_value:",min_value)
-            
-            
             if  (evaluation(min_value,max_value)==True):
                 overlap_counter+=1
                 print("Overlap#",overlap_counter)
@@ -41,16 +31,12 @@ def over_lap_check(numbers):
 
         if len(list_check2)==2 and flag==True:
             flag=False
-        
-        
+            
         elif flag==False:
             list_check1.append(numbers[n])
        
         elif flag==True:
             list_check2.append(numbers[n])
-
-
-      
 
     number_of_evaluated=len(list_check1)+len(list_check2)
     if number_of_evaluated==4:
