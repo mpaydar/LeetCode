dd3f5c5b5b505e50b1fca4c3d141618e5d0b48ed
mpaydar <m21newyork@gmail.com>
2023-02-01 22:31:34 -0500

    created and approached the problem through a more straight forward way

--- AdditionTwoInteger.py
+++ AdditionTwoInteger.py
@@ -1,54 +1,62 @@
-list1=[9,9,9,9,9,9,9]
-list2=[9,9,9,9]
+list1=[9,9,9,9,9,9,9] # 0 1 2 3 4 5 6
+list2=[9,9,9,9]   # 0 1 2 3
 i=list1.index(list1[-1])
 dynamic_answer=0
 stack=[]
 quotient=0
 index=0
 carry_list=[]
-def calculate_carryOver(dynamic_answer,carry):
-    # addition begins
-        if len(carry_list) == 0:
-            stack.append(dynamic_answer%10)
-            carry_list.append(carry)
-        else:
-            current_carry=carry_list[-1]
-            if current_carry != 0:
-                remainder = dynamic_answer % 10
-                new_value=remainder+current_carry
-                stack.append(new_value)
-            else:
-                remainder = dynamic_answer % 10
-                carry_list.append(carry)
-                stack.append(remainder)
-
-
-
-
-    # if remainder == 0 and 1<quotient:
-    #     stack.append(remainder)
-    #     print(stack)
-    #
-    # if carry == 0:
-    #     stack.append(dynamic_answer)
-    # if carry != 0:
-    #     stack.append(dynamic_answer+carry)
 
 
 
 
 
+def convertListToInteger(l1,l2):
+    combined_list=[]
+    convert_list=[]
+    # combine 2 list in one list:
+    combined_list.append(l1)
+    combined_list.append(l2)
+    for current_list in combined_list:
+        l=0
+        the_number=""
+        while l< len(current_list):
+            the_number+=str(current_list[l])
+            l+=1
+        convert_list.append(the_number)
+    return  convert_list
+
+def sumTheList(intList):
+    sum=0
+    for i in intList:
+        sum+=int(i)
+    return str(sum)
+
+def reverseString(n):
+    index=len(n)-1
+    reverseList=[]
+    while 0<=index:
+        reverseList.append(n[index])
+        index-=1
+    return reverseList
 
 
 
-while 0 <= i:
-    dynamic_answer=list2[i] + list1[i]
-    if 1<len(str(dynamic_answer)):
-        carry_over=quotient = dynamic_answer // 10
-        calculate_carryOver(dynamic_answer,carry_over)
-    else:
-        carry_over=quotient = dynamic_answer // 10
-        calculate_carryOver(dynamic_answer,carry_over)
-    i-=1
-print(stack)
+integerList=convertListToInteger(list1,list2)
+sumString=sumTheList(integerList)
+rList=reverseString(sumString)
+print(rList)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
